799bba7d5e3254db2ac5931376323f234ecc8d26
ybespalava <ybespalava@gmail.com>
2021-03-22 20:54:54 +0300

    feat: add FullScreen action

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.querySelector('.fullscreen').addEventListener('click', toggleFullScreen); //openfullscreen
 
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen(); //enter
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen(); //exit
+      }
+    }
+  }
